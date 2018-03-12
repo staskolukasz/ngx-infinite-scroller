94159b91c0dc769ccb6f16a88095b81feba62172
staskolukasz <stasko.lukasz@gmail.com>
2018-03-12 20:20:42 +0100

    Browser module removed

--- src/app/ngx-infinite-scroller.module.ts
+++ src/app/ngx-infinite-scroller.module.ts
@@ -1,5 +1,4 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
 
 import { NgxInfiniteScrollerDirective } from './ngx-infinite-scroller.directive';
 import { DirectiveStateService } from './directive-state.service';
@@ -9,7 +8,6 @@ import { DirectiveStateService } from './directive-state.service';
     NgxInfiniteScrollerDirective
   ],
   imports: [
-    BrowserModule
   ],
   exports: [
     NgxInfiniteScrollerDirective
